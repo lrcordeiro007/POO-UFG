c4046d3ed3da5dc3d2b421ce59445323471a1035
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-03-27 00:29:01 -0300

    Intervalo.java

--- EX1/Intervalo.java
+++ EX1/Intervalo.java
@@ -1,1 +1,71 @@
+public class Intervalo {
+
+    int numi;
+    int numf;
+
+    Intervalo (int numi, int numf) {
+        if (numi < numf) {
+            this.numi = numi;
+            this.numf = numf;
+        }
+        else{
+            this.numi = numf;
+            this.numf = numi;
+        }
+    }
+
+    double soma() {
+        double soma = 0;
+        for (int i = numi; i < numf + 1; i++) {
+            soma = soma + i;
+        }
+        return soma;
+    }
+
+    double media() {
+        return soma()/(numf-numi+1);
+    }
+
+    int qtdePares() {
+        int qtde = 0;
+        for (int i = numi; i < numf + 1; i++) {
+            if ((i % 2) == 0)
+                qtde++;
+        }
+        return qtde;
+    }
+
+    int qtdeImpares() {
+        int qtde = 0;
+        for (int i = numi; i < numf + 1; i++) {
+            if ((i % 2) > 0)
+                qtde++;
+        }
+        return qtde;
+    }
+
+    int qtdeIntervalo(){
+        int qtde = numf - numi + 1;
+        return qtde;
+        
+    }
+
+    double somaInicial(){
+        double soma = 0 ;
+        for(int i =numi +1;i <numf +1 ;i++){
+            soma = soma + i ;
+        }
+        return soma;
+    }
+
+    public String toString(){
+        return "Intervalo entre "+ numi + " e "+ numf;
+    }
+
+    int diferença(){
+        int dif =0;
+        dif= numf - numi;
+        return dif;
+    }
+}
 
